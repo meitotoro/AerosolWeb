e29d39a7601103acb1845a70dab69b75e5ce7a31
mei <mei.totoro@163.com>
2017-12-14 16:44:34 +0800

    修改了画图区域的版式

--- js/aodMap.js
+++ js/aodMap.js
@@ -1,8 +1,28 @@
-$(function(){
-    var year_list=$("#tablist #years a");
-    year_list.click(function(){
-        jquery.get(path,{
-            
-        })
+$(function () {
+    var year_list = $("#years-dropdown a");
+    var month_list=$("#months-dropdown a");
+    var input_month = $("months-dropdown").val();
+    month_list.click(function () {
+        var input_month=$("#input-month").val();
+        if (input_month != "请选择月份") {
+            var input_year = $("#input-year").val();
+            var year = parseInt(input_year.substring(0, 4));
+            var input_month=$(this).text();
+            var month=parseInt(input_month.slice(0,-1));
+            if (typeof (year) == "number") {
+                jQuery.get(path_map, {
+                    year:year,
+                    month:month
+                },function(data,textStatus){
+                    
+                    var img_html='<img id="imgAOD" src="http://192.168.1.127:9000/map?year='+year.toString()+'&month='+month.toString()+'" alt="aod_image">'
+                    $("#aeroMaps img").remove();
+                    $("#aeroMaps").append(img_html);
+                })
+
+            }
+
+        }
+
     })
 })
