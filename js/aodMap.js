c1492eeb387eed8d72e9a8351bab3a4f44c9fcb0
mei <mei.totoro@163.com>
2017-12-19 16:46:27 +0800

    修改页面版式

--- js/aodMap.js
+++ js/aodMap.js
@@ -14,12 +14,26 @@ $(function () {
                     year:year,
                     month:month
                 },function(data,textStatus){
-                    var sites_aod=data.sites_aod;
+                    var sites_aod=data.aod;
                     var filename=data.filename;
                     var img_html='<img id="imgAOD" src="http://192.168.1.127:9000/map/'+filename+'" alt="aod_image">';
-                    
+                    //画图
                     $("#aeroMaps img").remove();
                     $("#aeroMaps").append(img_html);
+                    //填充表格
+                    var sites=sites_aod.sites;
+                    var aod=new Array();
+                    for(var i=0;i<sites_aod.aod.length;i++){
+                        aod.push(parseFloat(sites_aod.aod[i]).toFixed(2))
+                    }
+                    $("#aodTable table tbody").html("");
+                    for(var i=0;i<sites.length;i++){
+                        var index=i+1;
+                        var html="<tr><th scope='row'>"+index+"</th><td>"+sites[i]+"</td><td>"+aod[i]+"</td></tr>";
+                        $("#aodTable table tbody").append(html);
+                        
+                    }
+                   
                 })
 
             }
