4e13497616bc886d66217d57812c596fa6882cdd
mei <mei.totoro@163.com>
2017-12-26 16:46:04 +0800

    继续修改页面版式，下一步加上进度条

--- js/aodMap.js
+++ js/aodMap.js
@@ -1,44 +1,76 @@
 $(function () {
     var year_list = $("#years-dropdown a");
-    var month_list=$("#months-dropdown a");
-    var input_month = $("months-dropdown").val();
+    var month_list = $("#months-dropdown a");
+    var input_month = $("input-month").val();
     month_list.click(function () {
-        var input_month=$("#input-month").val();
+        var input_month = $("#input-month").val();
         if (input_month != "请选择月份") {
             var input_year = $("#input-year").val();
             var year = parseInt(input_year.substring(0, 4));
-            var input_month=$(this).text();
-            var month=parseInt(input_month.slice(0,-1));
+            var input_month = $(this).text();
+            var month = parseInt(input_month.slice(0, -1));
             if (typeof (year) == "number") {
-                jQuery.get(path_map, {
-                    year:year,
-                    month:month
-                },function(data,textStatus){
-                    var sites_aod=data.aod;
-                    var filename=data.filename;
-                    var img_html='<img id="imgAOD" src="http://192.168.1.127:9000/map/'+filename+'" alt="aod_image">';
+                function reqListener() {
+                    var data=this.responseText;
+                    data=jQuery.parseJSON(data);
+                    var sites_aod = data.aod;
+                    var filename = data.filename;
+                    var img_html = '<img id="imgAOD" src="' + path_map +"/"+ filename + '" alt="aod_image">';
                     //画图
                     $("#aeroMaps img").remove();
                     $("#aeroMaps").append(img_html);
                     //填充表格
-                    var sites=sites_aod.sites;
-                    var aod=new Array();
-                    for(var i=0;i<sites_aod.aod.length;i++){
+                    var sites = sites_aod.sites;
+                    var aod = new Array();
+                    for (var i = 0; i < sites_aod.aod.length; i++) {
                         aod.push(parseFloat(sites_aod.aod[i]).toFixed(2))
                     }
                     $("#aodTable table tbody").html("");
-                    for(var i=0;i<sites.length;i++){
-                        var index=i+1;
-                        var html="<tr><th scope='row'>"+index+"</th><td>"+sites[i]+"</td><td>"+aod[i]+"</td></tr>";
+                    for (var i = 0; i < sites.length; i++) {
+                        var index = i + 1;
+                        var html = "<tr><th scope='row'>" + index + "</th><td>" + sites[i] + "</td><td>" + aod[i] + "</td></tr>";
                         $("#aodTable table tbody").append(html);
-                        
-                    }
-                   
-                })
 
+                    }
+                }
+
+                var oReq = new XMLHttpRequest();
+                oReq.addEventListener("load", reqListener);
+                var url = path_map + "?year=" + year + "&month=" + month;
+                oReq.open("GET", url);
+                oReq.send();
             }
 
-        }
+           /*   if (typeof (year) == "number") {
+                jQuery.get()(path_map, {
+                    year: year,
+                    month: month
+                }, function (data, textStatus) {
+                    var sites_aod = data.aod;
+                    var filename = data.filename;
+                    var img_html = '<img id="imgAOD" src=' + path_map + filename + '" alt="aod_image">';
+                    //画图
+                    $("#aeroMaps img").remove();
+                    $("#aeroMaps").append(img_html);
+                    //填充表格
+                    var sites = sites_aod.sites;
+                    var aod = new Array();
+                    for (var i = 0; i < sites_aod.aod.length; i++) {
+                        aod.push(parseFloat(sites_aod.aod[i]).toFixed(2))
+                    }
+                    $("#aodTable table tbody").html("");
+                    for (var i = 0; i < sites.length; i++) {
+                        var index = i + 1;
+                        var html = "<tr><th scope='row'>" + index + "</th><td>" + sites[i] + "</td><td>" + aod[i] + "</td></tr>";
+                        $("#aodTable table tbody").append(html);
+
+                    }
+
+                })
+
+            } */
+
+        } 
 
     })
 })
--- js/upStatic.js
+++ js/upStatic.js
@@ -25,11 +25,12 @@ $(function(){
             $("#input-month").val("请选择月份");
             
         }else if(index==1){
-            //选择年份统计，季平均统计框隐藏
+            //选择季节统计，月平均统计框隐藏
             $(".month").hide();
-            $(".year").hide();
+            $(".year").show();
             $(".season").show();
             $(".input-season").val("请选择季节");
+            $(".input-year").val("请选择年份");
             
         }else{
             $(".month").hide();
@@ -54,36 +55,5 @@ $(function(){
         $("#input-season").val(value);
 
     });
-     $(".statis .dropdown-menu a").click(function(){
-        
-        $("#input-group-year").show();
-        $("#input-year").val("请选择年份");        
-        $("#input-group-month").show();
-        $("#input-month").val("请选择月份");
-        $("#input-group-season").hide();        
-    });
-    /*$("#years-dropdown a").click(function(){
-        var value=$(this).text();
-        $("#input-year").val(value);
-    });
-    $("#months-dropdown a").click(function(){
-        var value=$(this).text();
-        $("#input-month").val(value);
-    });
-    $("#season-dropdown a").click(function(){
-        var value=$(this).text();
-        $("#input-season").val(value);
-    });
-    $("#radioSeason").click(function(){
-        $("#input-group-season").show(); 
-        $("#input-season").val("请选择季节");       
-        $("#input-group-year").hide();
-        $("#input-group-month").hide();
-    });
-    $("#radioyear").click(function(){
-        $("#input-group-year").show();
-        $("#input-year").val("请选择年份"); 
-        $("#input-group-month").hide();
-        $("#input-group-season").hide(); 
-    }) */
+    
 })
