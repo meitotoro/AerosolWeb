b0e021ee77b0860bd7009a50dac21d45c6702846
mei <mei.totoro@163.com>
2018-01-17 17:29:37 +0800

    修改页面版式，背景图等

--- js/aodMap.js
+++ js/aodMap.js
@@ -2,7 +2,7 @@ $(function () {
     var year_list = $("#years-dropdown a");
     var month_list = $("#months-dropdown a");
     var input_month = $("input-month").val();
-    var wait=$(".wait");
+    var wait = $(".wait");
     wait.hide();
     month_list.click(function () {
         var input_month = $("#input-month").val();
@@ -14,19 +14,21 @@ $(function () {
             if (typeof (year) == "number") {
                 function reqListener() {
                     wait.hide();
-                    var data=this.responseText;                    
-                    data=jQuery.parseJSON(data);
+                    var data = this.responseText;
+                    data = jQuery.parseJSON(data);
                     var sites_aod = data.aod;
-                    if(sites_aod==""){
-                        var alertStr="数据库中没有"+year+"年"+input_month+"的数据!";
+                    var sites = sites_aod.sites;
+                    var locate = sites_aod.locate;
+                    if (sites_aod == "") {
+                        var alertStr = "数据库中没有" + year + "年" + input_month + "的数据!";
                         alert(alertStr);
                         return;
                     }
                     var filename = data.filename;
-                    var china_img_html = '<img id="imgAOD" src="' + path_map +"/"+ filename[0] + '" alt="aod_image">';
-                    var jingjinji_img_html='<img id="jingjinjiAOD" src="' + path_map +"/"+ filename[1] + '" alt="aod_image">';
-                    var changsanjiao_img_html='<img id="changsanjiaoAOD" src="' + path_map +"/"+ filename[2] + '" alt="aod_image">';
-                    var zhusanjiao_img_html='<img id="zhusanjiaoAOD" src="' + path_map +"/"+ filename[3] + '" alt="aod_image">';
+                    var china_img_html = '<img id="imgAOD" src="' + path_map + "/" + filename[0] + '" alt="aod_image">';
+                    var jingjinji_img_html = '<img id="jingjinjiAOD" src="' + path_map + "/" + filename[1] + '" alt="aod_image">';
+                    var changsanjiao_img_html = '<img id="changsanjiaoAOD" src="' + path_map + "/" + filename[2] + '" alt="aod_image">';
+                    var zhusanjiao_img_html = '<img id="zhusanjiaoAOD" src="' + path_map + "/" + filename[3] + '" alt="aod_image">';
                     //画图
                     $(".AODMaps div img").remove();
                     $("#chinaMaps").append(china_img_html);
@@ -35,18 +37,30 @@ $(function () {
                     $("#zhusanjiaoMap").append(zhusanjiao_img_html);
                     $(".AODMaps div img").show();
                     //填充表格
-                    var sites = sites_aod.sites;
+
                     var aod = new Array();
                     for (var i = 0; i < sites_aod.aod.length; i++) {
                         aod.push(parseFloat(sites_aod.aod[i]).toFixed(2))
                     }
                     $("#aodTable table tbody").html("");
                     for (var i = 0; i < sites.length; i++) {
+                        var lon = Math.round(locate[i][0]).toString();
+                        var lat = Math.round(locate[i][1]).toString();
+                        var locate_Html = "<td>" + lon + "°E," + lat + "°N</td>";
                         var index = i + 1;
-                        var html = "<tr><th scope='row'>" + index + "</th><td>" + sites[i] + "</td><td>" + aod[i] + "</td></tr>";
+                        var b = index % 2;
+                        var html = "";
+                        if (b == 0) {
+                            //设置偶数行的背景色
+                            html = "<tr bgcolor='#bed1f2'><td>" + index + "</td><td>" + sites[i] + "</td>" + locate_Html + "<td>" + aod[i] + "</td></tr > ";
+                        }
+                        else if (b == 1) {
+                            html = "<tr bgcolor=><td>" + index + "</td><td>" + sites[i] + "</td>" + locate_Html + "<td>" + aod[i] + "</td></tr>";
+                        }
                         $("#aodTable table tbody").append(html);
-
                     }
+                    //设置表格的padding
+                    $("#aodTable table tbody tr td").css({ "padding": "5px 10px 5px 10px", "color": "#4d4d4d" });
                 }
 
                 var oReq = new XMLHttpRequest();
@@ -59,36 +73,36 @@ $(function () {
                 $(".AODMaps div img").hide();
             }
 
-           /*   if (typeof (year) == "number") {
-                jQuery.get()(path_map, {
-                    year: year,
-                    month: month
-                }, function (data, textStatus) {
-                    var sites_aod = data.aod;
-                    var filename = data.filename;
-                    var img_html = '<img id="imgAOD" src=' + path_map + filename + '" alt="aod_image">';
-                    //画图
-                    $("#aeroMaps img").remove();
-                    $("#aeroMaps").append(img_html);
-                    //填充表格
-                    var sites = sites_aod.sites;
-                    var aod = new Array();
-                    for (var i = 0; i < sites_aod.aod.length; i++) {
-                        aod.push(parseFloat(sites_aod.aod[i]).toFixed(2))
-                    }
-                    $("#aodTable table tbody").html("");
-                    for (var i = 0; i < sites.length; i++) {
-                        var index = i + 1;
-                        var html = "<tr><th scope='row'>" + index + "</th><td>" + sites[i] + "</td><td>" + aod[i] + "</td></tr>";
-                        $("#aodTable table tbody").append(html);
+            /*   if (typeof (year) == "number") {
+                 jQuery.get()(path_map, {
+                     year: year,
+                     month: month
+                 }, function (data, textStatus) {
+                     var sites_aod = data.aod;
+                     var filename = data.filename;
+                     var img_html = '<img id="imgAOD" src=' + path_map + filename + '" alt="aod_image">';
+                     //画图
+                     $("#aeroMaps img").remove();
+                     $("#aeroMaps").append(img_html);
+                     //填充表格
+                     var sites = sites_aod.sites;
+                     var aod = new Array();
+                     for (var i = 0; i < sites_aod.aod.length; i++) {
+                         aod.push(parseFloat(sites_aod.aod[i]).toFixed(2))
+                     }
+                     $("#aodTable table tbody").html("");
+                     for (var i = 0; i < sites.length; i++) {
+                         var index = i + 1;
+                         var html = "<tr><th scope='row'>" + index + "</th><td>" + sites[i] + "</td><td>" + aod[i] + "</td></tr>";
+                         $("#aodTable table tbody").append(html);
+ 
+                     }
+ 
+                 })
+ 
+             } */
 
-                    }
-
-                })
-
-            } */
-
-        } 
+        }
 
     })
 })
