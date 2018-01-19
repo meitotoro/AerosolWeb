d2fe6a47a6b049f8bf4e57e4c0d7d41dd5b851d8
mei <mei.totoro@163.com>
2018-01-19 09:41:20 +0800

    修改表格版式

--- js/aodMap.js
+++ js/aodMap.js
@@ -50,17 +50,38 @@ $(function () {
                         var index = i + 1;
                         var b = index % 2;
                         var html = "";
+                        var percentage=parseInt(aod[i]/1.5*100)+"%";
+                        var backgroundColor="";
+                        var percent=aod[i]/1.5;
+                        if(percent<0.2){
+                            backgroundColor="rgba(51, 153, 255)";
+                        }
+                        else if(0.2<=percent<0.4){
+                            backgroundColor="rgba(0, 255, 255)";
+                        }else if(0.4<=percent<0.6){
+                            backgroundColor="rgba(200, 215, 168)";
+                        }
+                        else if(0.6<=percent<0.8){
+                            backgroundColor="rgba(230, 172, 56)";
+                        }else{
+                            backgroundColor="rgba(255, 0, 0)";
+                        }
                         if (b == 0) {
                             //设置偶数行的背景色
-                            html = "<tr bgcolor='#bed1f2'><td>" + index + "</td><td>" + sites[i] + "</td>" + locate_Html + "<td>" + aod[i] + "</td></tr > ";
+                            html = "<tr bgcolor='#bed1f2'><td>" + index + "</td><td>" + sites[i] + "</td>" + locate_Html + "<td style='background-size: "+percentage+"'>" + aod[i] + "</td></tr > ";
                         }
                         else if (b == 1) {
-                            html = "<tr bgcolor=><td>" + index + "</td><td>" + sites[i] + "</td>" + locate_Html + "<td>" + aod[i] + "</td></tr>";
+                            html = "<tr><td>" + index + "</td><td>" + sites[i] + "</td>" + locate_Html +  "<td style='background-size: "+percentage+"'>"+ aod[i] + "</td></tr>";
                         }
                         $("#aodTable table tbody").append(html);
                     }
                     //设置表格的padding
-                    $("#aodTable table tbody tr td").css({ "padding": "5px 10px 5px 10px", "color": "#4d4d4d" });
+                    $("#aodTable table tbody tr td:last-child").css({ "padding": "5px 10px 5px 10px", "color": "#4d4d4d",
+                    "background-color": backgroundColor,
+                    "background-repeat": "no-repeat" });
+                    $("#aodTable table tbody tr:last-child td:first-child").css({"border-bottom-left-radius":"10px"});
+                    $("#aodTable table tbody tr:last-child td:last-child").css({"border-bottom-right-radius":"10px"});
+
                 }
 
                 var oReq = new XMLHttpRequest();
